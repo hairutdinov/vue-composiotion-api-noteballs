4a563d6c725f23fb9ce5cbdf09e80422a3163216
bulat <bulatxajrutdinoff@yandex.ru>
2022-11-13 10:06:23 +0300

    design

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
-<script setup>
-
-</script>
-
 <template>
-  <RouterLink to="/">Notes</RouterLink> |
-  <RouterLink to="/stats">Stats</RouterLink>
-  <RouterView />
+  <NavBar />
+
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+import NavBar from '@/components/Layout/NavBar.vue'
+</script>
 
+<style>
+@import 'bulma/css/bulma.min.css';
 </style>

a877bedb6690603174eb4f9ac707038cddb0f19d
bulat <bulatxajrutdinoff@yandex.ru>
2022-11-13 10:36:56 +0300

    refactoring + Note component

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -5,10 +5,10 @@
       <div class="field">
         <div class="control">
           <textarea
-            v-model="newNote"
-            class="textarea"
-            placeholder="Add a new note"
-            ref="newNoteRef"
+              v-model="newNote"
+              class="textarea"
+              placeholder="Add a new note"
+              ref="newNoteRef"
           />
         </div>
       </div>
@@ -16,9 +16,9 @@
       <div class="field is-grouped is-grouped-right">
         <div class="control">
           <button
-            @click="addNote"
-            :disabled="!newNote"
-            class="button is-link has-background-success"
+              @click="addNote"
+              :disabled="!newNote"
+              class="button is-link has-background-success"
           >
             Add New Note
           </button>
@@ -27,63 +27,53 @@
     </div>
 
     <Note
-      v-for="note in notes"
-      :key="note.id"
-      :note="note"
-      @deleteClicked="deleteNote"
+        v-for="note in notes"
+        :key="note.id"
+        :note="note"
+        @deleteClicked="deleteNote"
     />
 
   </div>
 </template>
 
 <script setup>
+import {ref} from 'vue'
+import Note from '@/components/Notes/Note.vue'
 
-/*
-  imports
-*/
+const newNote = ref('')
+const newNoteRef = ref(null)
 
-  import { ref } from 'vue'
-  import Note from '@/components/Notes/Note.vue'
+const notes = ref([
+  {
+    id: 'id1',
+    content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.'
+  },
+  {
+    id: 'id2',
+    content: 'This is a shorter note! Woo!'
+  }
+])
 
-/*
-  notes
-*/
+const addNote = () => {
+  let currentDate = new Date().getTime(),
+      id = currentDate.toString()
 
-  const newNote = ref('')
-  const newNoteRef = ref(null)
-
-  const notes = ref([
-    {
-      id: 'id1',
-      content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.'
-    },
-    {
-      id: 'id2',
-      content: 'This is a shorter note! Woo!'
-    }
-  ])
-
-  const addNote = () => {
-    let currentDate = new Date().getTime(),
-        id = currentDate.toString()
-
-    let note = {
-      id,
-      content: newNote.value
-    }
-
-    notes.value.unshift(note)
-    newNote.value = ''
-    newNoteRef.value.focus()
+  let note = {
+    id,
+    content: newNote.value
   }
 
-/*
-  delete note
-*/
+  notes.value.unshift(note)
+  newNote.value = ''
+  newNoteRef.value.focus()
+}
 
-  const deleteNote = idToDelete => {
-    notes.value = notes.value.filter(note => { return note.id !== idToDelete })
-  }
+
+const deleteNote = idToDelete => {
+  notes.value = notes.value.filter(note => {
+    return note.id !== idToDelete
+  })
+}
 
 </script>
 
